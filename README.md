d48c44a45bf09b7a6b921c46266f4b2bb9683833
wzz <ywc0008@naver.com>
2024-02-26 16:58:30 +0900

    express, nodemon, babel 설정

---
+++ README.md
@@ -0,0 +1,12 @@
+# Woom
+
+Zoom Clone using NodeJS, WebRTC and Websockets.
+
+npm i nodemon -D
+npm i @babel/core @babel/cli @babel/node -D
+npm i @babel/preset-env -D
+npm i express
+npm i pug
+
+app.js는 프론트엔드
+server.js는 백엔드 코드가 작동
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import http from "http";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`listening on http://localhost:3000`);
+
+const server = http.createServer(app);
